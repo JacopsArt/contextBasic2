30cd71b288a6dadfdc041f4665d1ea99332dc829
Jermaine <jermaine.aroma@gmail.com>
2024-01-30 22:25:50 +0100

    Created LiibaryContextProvider to manage all states and functions. Cleared library from overbose code

--- src/Library.jsx
+++ src/Library.jsx
@@ -1,28 +1,14 @@
-import { useState } from "react";
-import { collection } from "./collection";
+import React from "react";
+import LibraryContextProvider from "./LibraryContextProvider";
 import { Books } from "./Books";
-import { LibraryContext } from "./LibraryContext";
 
 function Library() {
-  const [books, setBooks] = useState(collection.books);
-  const borrowBook = (id) => {
-    const newBooks = books.map((book) =>
-      book.id === id ? { ...book, available: false } : book
-    );
-    return setBooks(newBooks);
-  };
-  const returnBook = (id) => {
-    const newBooks = books.map((book) =>
-      book.id === id ? { ...book, available: true } : book
-    );
-    return setBooks(newBooks);
-  };
   return (
     <div className="App">
       <h1>Library</h1>
-      <LibraryContext.Provider value={{ books, borrowBook, returnBook }}>
+      <LibraryContextProvider>
         <Books />
-      </LibraryContext.Provider>
+      </LibraryContextProvider>
     </div>
   );
 }
